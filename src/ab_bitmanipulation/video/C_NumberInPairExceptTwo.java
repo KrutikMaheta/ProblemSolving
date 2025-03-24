fbf9e64ed0d3ad6c8d2fc572c25e518ff7c4d63a
Krutik <krutik.maheta@gmail.com>
2025-03-24 22:03:12 +0530

    Corrections

--- src/ab_bitmanipulation/video/C_NumberInPairExceptTwo.java
+++ src/ab_bitmanipulation/video/C_NumberInPairExceptTwo.java
@@ -1,42 +1,46 @@
+//  https://leetcode.com/problems/single-number-iii/
+
 package ab_bitmanipulation.video;
 
 public class C_NumberInPairExceptTwo {
 
-	public static void main(String[] args) {
-		int arr[] = { 9, 1, 1, 2, 3, 5, 3, 2, 7, 8, 8, 9 };
-		numberInPairExceptTwo(arr);
-	}
+    public static void main(String[] args) {
+        int arr[] = {9, 1, 1, 2, 3, 5, 3, 2, 7, 8, 8, 9};
+        numberInPairExceptTwo(arr);
+    }
 
-	private static void numberInPairExceptTwo(int[] arr) {
-		int XOR = 0;
-		
-		//	First find the XORs of all the numbers
-		//	The final result will be XOR of asked 2 numbers
-		for (int i = 0; i < arr.length; i++) {
-			XOR ^= arr[i];
-		}
+    private static void numberInPairExceptTwo(int[] arr) {
+        int XOR = 0;
 
-		//	Find out the first SET bit in the XORed number
-		int k = 0;
-		while (true) {
-			if ((XOR & (1 << k)) > 0)
-				break;
-			k++;
-		}
+        //	First find the XORs of all the numbers
+        //	The final result will be XOR of asked 2 numbers
+        for (int i = 0; i < arr.length; i++) {
+            XOR ^= arr[i];
+        }
 
-		int num1 = 0;
-		int num2 = 0;
+        //	Find out the first SET bit in the XORed number
+        int k = 0;
+        while (true) {
+            if ((XOR & (1 << k)) != 0) {
+                break;
+            }
+            k++;
+        }
 
-		//	Separate out the numbers based on the kth bit
-		//	kth bit will always be different in both the required numbers.
-		for (int i = 0; i < arr.length; i++) {
-			if ((arr[i] & (1 << k)) > 0)
-				num1 ^= arr[i];
-			else
-				num2 ^= arr[i];
-		}
+        int num1 = 0;
+        int num2 = 0;
 
-		System.out.println(num1 + "," + num2);
-	}
+        //	Separate out the numbers based on the kth bit
+        //	kth bit will always be different in both the required numbers.
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] & (1 << k)) != 0) {
+                num1 ^= arr[i];
+            } else {
+                num2 ^= arr[i];
+            }
+        }
+
+        System.out.println(num1 + "," + num2);
+    }
 
 }
